d02d64abef4b810f9e755299754a55943c04bff7
zacanger <zac@zacanger.com>
2018-02-16 23:07:51 -0700

    Add the thing

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-module.exports = {}
+var cluster = require('cluster')
+var cpus = require('os').cpus
+
+var len = cpus().length
+
+module.exports = function boringCluster (mod) {
+  if (cluster.isMaster) {
+    for (var i = 0; i < len; i++) {
+      cluster.fork()
+    }
+
+    cluster.on('exit', function (worker) {
+      console.log(worker.process.pid + ' died; forking.')
+      cluster.fork()
+    })
+  } else {
+    require(mod)
+  }
+}
